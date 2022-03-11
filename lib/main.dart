08f61a422b0a90dd6aafcab07e157694798cd1b8
nilamroychoudhury1999 <nilamroychoudhury16@gmail.com>
2022-03-11 22:20:14 +0530

    tut3

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(primarySwatch: Colors.purple),
   ));
 }
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      child: Text('hi'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Aesome App'),
+      ),
+      body: Container(
+        child: Text('hi'),
+      ),
     );
   }
 }
