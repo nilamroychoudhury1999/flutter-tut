db1933eacec7399daac43f8eb1ddf25c0eab8fd6
nilamroychoudhury1999 <nilamroychoudhury16@gmail.com>
2022-03-13 13:05:34 +0530

    tut11

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,11 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 import '../drawer.dart';
 import '../name_card_widget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,25 +13,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var textP = "change my name";
-  TextEditingController _nameController = TextEditingController();
+  //var textP = "change my name";
+  //TextEditingController _nameController = TextEditingController();
+
+  final Uri url = Uri.parse("https://jsonplaceholder.typicode.com/photos");
+  var data;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Aesome App'),
       ),
-      body: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: NameCard(textP: textP, nameController: _nameController),
-        ),
-      )),
+      body: data != null
+          ? ListView.builder(
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(data[index]["title"]),
+                  subtitle: Text("${data[index]["id"]}"),
+                  leading: Image.network(data[index]["url"]),
+                );
+              },
+              itemCount: data.length,
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          textP = _nameController.text;
+          //  textP = _nameController.text;
           setState(() {});
         },
         child: Icon(Icons.send),
